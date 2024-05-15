0c55ec561a7446144b55dd6da4a29e4a556a3eff
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-15 15:10:21 +0200

    request class & user service first implmentation

--- src/main/java/edu/yacoubi/usermanagement/registration/RegistrationRequest.java
+++ src/main/java/edu/yacoubi/usermanagement/registration/RegistrationRequest.java
@@ -1,4 +1,16 @@
-package edu.yacoubi.usermanagement.user;
+package edu.yacoubi.usermanagement.registration;
 
+import edu.yacoubi.usermanagement.user.Role;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class RegistrationRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean isEnabled = false;
+    private List<Role> roles;
 }
--- src/main/java/edu/yacoubi/usermanagement/user/UserService.java
+++ src/main/java/edu/yacoubi/usermanagement/user/UserService.java
@@ -1,2 +1,38 @@
-package edu.yacoubi.usermanagement.user;public class UserService {
+package edu.yacoubi.usermanagement.user;
+
+import edu.yacoubi.usermanagement.registration.RegistrationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+@RequiredArgsConstructor
+public class UserService implements IUserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public User registerUser(RegistrationRequest registrationRequest) {
+        return null;
+    }
+
+    @Override
+    public void updateUser(Long id, String firstName, String lastName, String email) {
+
+    }
 }
