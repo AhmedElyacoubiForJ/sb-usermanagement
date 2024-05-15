6b957681c51a2b9047fd7766f2aae8e003675fa2
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-15 15:06:57 +0200

    user service contract

--- src/main/java/edu/yacoubi/usermanagement/user/IUserService.java
+++ src/main/java/edu/yacoubi/usermanagement/user/IUserService.java
@@ -1,2 +1,14 @@
-package edu.yacoubi.usermanagement.user;public interface IUserService {
+package edu.yacoubi.usermanagement.user;
+
+import edu.yacoubi.usermanagement.registration.RegistrationRequest;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserService {
+    List<User> getAllUsers();
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
+    User registerUser(RegistrationRequest registrationRequest);
+    void updateUser(Long id, String firstName, String lastName, String email);
 }
