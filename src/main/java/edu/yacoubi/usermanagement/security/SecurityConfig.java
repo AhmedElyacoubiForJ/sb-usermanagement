f245838db99a371487e553b610db5ce0bd82b552
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-16 11:46:49 +0200

    can register user to db

--- src/main/java/edu/yacoubi/usermanagement/security/SecurityConfig.java
+++ src/main/java/edu/yacoubi/usermanagement/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/", "/error","/registration/**")
+                .requestMatchers("/","/registration/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
