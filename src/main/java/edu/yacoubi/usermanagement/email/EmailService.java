791fcc2ae39ee59fb88412a83d63243f73dd9a1c
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2024-05-24 07:18:10 +0200

    todo a seperate mail service

--- src/main/java/edu/yacoubi/usermanagement/email/EmailService.java
+++ src/main/java/edu/yacoubi/usermanagement/email/EmailService.java
@@ -1,2 +1,5 @@
-package edu.yacoubi.usermanagement.email;public interface EmailService {
+package edu.yacoubi.usermanagement.email;
+
+public interface EmailService {
+    void sendHtmlEmail(String name, String to, String token);
 }
--- src/main/java/edu/yacoubi/usermanagement/email/EmailServiceImpl.java
+++ src/main/java/edu/yacoubi/usermanagement/email/EmailServiceImpl.java
@@ -1,2 +1,9 @@
-package edu.yacoubi.usermanagement.email;public class EmailServcieImpl {
+package edu.yacoubi.usermanagement.email;
+
+public class EmailServiceImpl implements EmailService {
+    
+    @Override
+    public void sendHtmlEmail(String name, String to, String token) {
+
+    }
 }
